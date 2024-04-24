1c1fa59e62613f258b4a555d1f699f0c6c19b2c3
barabarinov <barabarinov@gmail.com>
2024-04-24 12:18:45 +0300

    Fixed imports, created constant for css selector literal, serarated creation Product to few methods, changet class name

--- app/parse.py
+++ app/parse.py
@@ -1,36 +1,19 @@
-import csv
 import time
-from dataclasses import dataclass, astuple, fields
+from dataclasses import dataclass, fields
+from urllib.parse import urljoin
+
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from tqdm import tqdm
-from urllib.parse import urljoin
+
+from app.csv_writer import CSVFileWriter
+
 
 BASE_URL = "https://webscraper.io/"
-
-
-class CSVFileWriter:
-    def __init__(
-        self,
-        file_name: str,
-        column_fields: [str]
-    ) -> None:
-        self.file_name = file_name
-        self.column_fields = column_fields
-
-    def write_in_csv_file(self, data: list) -> None:
-        with open(
-            self.file_name + ".csv",
-            "w",
-            encoding="utf-8",
-            newline="",
-        ) as file:
-            writer = csv.writer(file)
-            writer.writerow(self.column_fields)
-            writer.writerows([astuple(record) for record in data])
+ECOMMERCE_SCROLL_MORE_SELECTOR = ".ecomerce-items-scroll-more"
 
 
 @dataclass
@@ -45,7 +28,7 @@ class Product:
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
 
 
-class WebScraper:
+class ECommerceScraper:
 
     def __init__(self) -> None:
         self.options = Options()
@@ -56,34 +39,49 @@ class WebScraper:
 
         return self.options
 
-    def _update_driver(self, product_url: str) -> None:
+    def _open_page(self, product_url: str) -> None:
         page_url = urljoin(BASE_URL, product_url)
         self.driver.get(page_url)
 
     @staticmethod
-    def _parse_single_product(product: WebElement) -> Product:
+    def _get_title(product: WebElement) -> str:
+        return product.find_element(
+            By.CSS_SELECTOR, ".caption > h4 > a"
+        ).get_attribute("title")
+
+    @staticmethod
+    def _get_description(product: WebElement) -> str:
+        return product.find_element(By.CSS_SELECTOR, ".caption > .card-text").text
+
+    @staticmethod
+    def _get_price(product: WebElement) -> float:
+        return float(product.find_element(
+            By.CSS_SELECTOR, ".caption > .price").text.replace("$", "")
+        )
+
+    @staticmethod
+    def _get_rating(product: WebElement) -> int:
+        return len(
+            product.find_elements(
+                By.CSS_SELECTOR, "div.ratings > p:nth-of-type(2) > span"
+            )
+        )
+
+    @staticmethod
+    def _get_num_of_reviews(product: WebElement) -> int:
+        return int(
+            product.find_element(
+                By.CSS_SELECTOR, "div.ratings > p.review-count"
+            ).text.split(" ")[0]
+        )
+
+    def _parse_single_product(self, product: WebElement) -> Product:
         return Product(
-            title=product.find_element(
-                By.CSS_SELECTOR, ".caption > h4 > a"
-            ).get_attribute("title"),
-            description=product.find_element(
-                By.CSS_SELECTOR, ".caption > .card-text"
-            ).text,
-            price=float(
-                product.find_element(
-                    By.CSS_SELECTOR, ".caption > .price")
-                .text.replace("$", "")
-            ),
-            rating=len(
-                product.find_elements(
-                    By.CSS_SELECTOR, "div.ratings > p:nth-of-type(2) > span"
-                )
-            ),
-            num_of_reviews=int(
-                product.find_element(
-                    By.CSS_SELECTOR, "div.ratings > p.review-count"
-                ).text.split(" ")[0]
-            ),
+            title=self._get_title(product),
+            description=self._get_description(product),
+            price=self._get_price(product),
+            rating=self._get_rating(product),
+            num_of_reviews=self._get_num_of_reviews(product),
         )
 
     def _accept_cookies(self) -> None:
@@ -98,7 +96,7 @@ class WebScraper:
     def _has_pagination(self) -> bool:
         try:
             pagination_button = self.driver.find_element(
-                By.CSS_SELECTOR, ".ecomerce-items-scroll-more"
+                By.CSS_SELECTOR, ECOMMERCE_SCROLL_MORE_SELECTOR
             )
             return pagination_button.is_displayed()
         except NoSuchElementException:
@@ -107,13 +105,13 @@ class WebScraper:
     def _scroll_whole_page(self) -> None:
         while self._has_pagination():
             more_button = self.driver.find_element(
-                By.CLASS_NAME, ".ecomerce-items-scroll-more"[1:]
+                By.CLASS_NAME, ECOMMERCE_SCROLL_MORE_SELECTOR[1:]
             )
             more_button.click()
             time.sleep(0.2)
 
     def get_all_products(self, product_url: str) -> list[Product]:
-        self._update_driver(product_url)
+        self._open_page(product_url)
         self._accept_cookies()
         self._scroll_whole_page()
         products = self.driver.find_elements(By.CSS_SELECTOR, ".card-body")
@@ -135,7 +133,7 @@ urls = {
 
 
 def get_all_products() -> None:
-    scraper = WebScraper()
+    scraper = ECommerceScraper()
 
     for file_name, url in urls.items():
         products = scraper.get_all_products(product_url=url)
